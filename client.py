f76051b83be93125d7e6dab9f0bdcdc09799063a
0xhtml <34682885+0xhtml@users.noreply.github.com>
2019-10-08 01:47:13 +0200

    Put stuff in their own functions

--- client.py
+++ client.py
@@ -32,43 +32,48 @@ class Client:
             elif xml.tag == 'room':
                 xmldata = xml.find('data')
                 if xmldata.get('class') == 'memento':
-                    xmlstate = xmldata.find('state')
-                    self.color = xmlstate.get('currentPlayerColor')
-                    self.turn = xmlstate.get('turn')
-
-                    xmlboard = xmlstate.find('board')
-                    self.board = {}
-                    for xmlfields in xmlboard.findall('fields'):
-                        for xmlfield in xmlfields.findall('field'):
-                            x = xmlfield.get('x')
-                            y = xmlfield.get('y')
-                            z = xmlfield.get('z')
-                            if xmlfield.get('isObstructed') == 'true':
-                                self.board[(x, y, z)] = 'OBSTRUCTED'
-                            else:
-                                self.board[(x, y, z)] = 'EMPTY'
-
-                    self.undeployed = {'RED': [], 'BLUE': []}
-
-                    xmlundeployedRed = xmlstate.find('undeployedRedPieces')
-                    for xmlpiece in xmlundeployedRed.findall('piece'):
-                        self.undeployed['RED'].append(xmlpiece.get('type'))
-
-                    xmlundeployedBlue = xmlstate.find('undeployedBluePieces')
-                    for xmlpiece in xmlundeployedBlue.findall('piece'):
-                        self.undeployed['BLUE'].append(xmlpiece.get('type'))
+                    self.parse_state(xmldata.find('state'))
                 elif xmldata.get('class') == 'sc.framework.plugins.protocol.MoveRequest':
-                    ret = b'<room roomId="' + self.room.encode() + b'">'
-                    ret += b'<data class="setmove">'
-                    ret += b'<piece owner="' + self.color.encode() + b'" type="ANT"/>'
-                    ret += b'<destination x="0" y="0" z="0"/>'
-                    ret += b'</data>'
-                    ret += b'</room>'
-                    self.socket.send(ret)
+                    self.send_move()
             elif xml.tag == 'left':
                 return False
 
             return True
+
+    def parse_state(self, xml):
+        self.color = xml.get('currentPlayerColor')
+        self.turn = xml.get('turn')
+
+        xmlboard = xml.find('board')
+        self.board = {}
+        for xmlfields in xmlboard.findall('fields'):
+            for xmlfield in xmlfields.findall('field'):
+                x = xmlfield.get('x')
+                y = xmlfield.get('y')
+                z = xmlfield.get('z')
+                if xmlfield.get('isObstructed') == 'true':
+                    self.board[(x, y, z)] = 'OBSTRUCTED'
+                else:
+                    self.board[(x, y, z)] = 'EMPTY'
+
+        self.undeployed = {'RED': [], 'BLUE': []}
+
+        xmlundeployedRed = xml.find('undeployedRedPieces')
+        for xmlpiece in xmlundeployedRed.findall('piece'):
+            self.undeployed['RED'].append(xmlpiece.get('type'))
+
+        xmlundeployedBlue = xml.find('undeployedBluePieces')
+        for xmlpiece in xmlundeployedBlue.findall('piece'):
+            self.undeployed['BLUE'].append(xmlpiece.get('type'))
+
+    def send_move(self):
+        ret = b'<room roomId="' + self.room.encode() + b'">'
+        ret += b'<data class="setmove">'
+        ret += b'<piece owner="' + self.color.encode() + b'" type="ANT"/>'
+        ret += b'<destination x="0" y="0" z="0"/>'
+        ret += b'</data>'
+        ret += b'</room>'
+        self.socket.send(ret)
 
     def join_any_game(self):
         self.send(b'<join gameType="swc_2020_hive"/>')
