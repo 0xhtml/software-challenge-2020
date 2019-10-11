c6e7d171675e3d0614f4afc58f3149a8a09f354c
0xhtml <34682885+0xhtml@users.noreply.github.com>
2019-10-11 23:47:01 +0200

    Fix bee setting rule

--- gamestate.py
+++ gamestate.py
@@ -38,7 +38,7 @@ class GameState:
                 return True
             dests = filter(f, dests)
 
-        if (self.turn > 5 and (self.color, "BEE") not in self.undeployed):
+        if (self.turn > 5 and (self.color, "BEE") in self.undeployed):
             types = {"BEE"}
         else:
             undeployed = filter(lambda x: x[0] == self.color, self.undeployed)
