8dd57ecc0e674d19fe5089d0f56e3325f05cb093
0xhtml <34682885+0xhtml@users.noreply.github.com>
2019-10-15 18:58:44 +0200

    Improve test

--- socha/tests/test_with_server.py
+++ socha/tests/test_with_server.py
@@ -8,12 +8,20 @@ def test_with_server():
     server = subprocess.Popen(["java", "-jar", "server.jar"], cwd="server", stdout=open("s.log", "w"), stderr=subprocess.STDOUT)
     time.sleep(5)
 
-    player = subprocess.Popen(["java", "-jar", "defaultplayer.jar"], cwd="server", stdout=open("p.log", "w"), stderr=subprocess.STDOUT)
-    time.sleep(5)
+    try:
+        for _ in range(2):
+            player = subprocess.Popen(["java", "-jar", "defaultplayer.jar"], cwd="server", stdout=open("p.log", "w"), stderr=subprocess.STDOUT)
+            time.sleep(5)
 
-    client = net.Client("localhost", 13050)
-    client.join_any_game()
-
-    player.wait()
-    server.terminate()
-    server.wait()
+            try:
+                client = net.Client("localhost", 13050)
+                client.join_any_game()
+            finally:
+                try:
+                    player.wait(5)
+                except subprocess.TimeoutExpired:
+                    player.terminate()
+                    player.wait()
+    finally:
+        server.terminate()
+        server.wait()
