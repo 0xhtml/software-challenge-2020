88de59d5bfa66643dc2e7a3ebbc95ebb55094571
0xhtml <34682885+0xhtml@users.noreply.github.com>
2020-01-10 22:54:28 +0100

    Improve

--- socha/players.py
+++ socha/players.py
@@ -12,54 +12,64 @@ class AlphaBeta:
     MAX_TIME = 1900000000
 
     def alphaBeta(self, gamestate: gamestate.GameState, depth: int, a, b):
-        if (depth <= 0 or time.time_ns() - self.now > self.MAX_TIME):
+        if (time.time_ns() - self.now > self.MAX_TIME):
+            self.timeout = True
+        if (depth <= 0 or self.timeout):
             return self.evaluate(gamestate)
-        best = -math.inf
         possible_moves = gamestate.get_possible_moves()
         for move in possible_moves:
-            next_gamestate = gamestate.clone()
-            next_gamestate = move.perform(next_gamestate)
-            value = -self.alphaBeta(next_gamestate, depth - 1, -b, -a)
-            if value > best:
-                if value >= b:
-                    return value
-                best = value
-                if depth == self.depth:
-                    self.best[depth] = (value, move)
-                if value > a:
-                    a = value
-        return best
+            value = -self.alphaBeta(
+                move.perform(gamestate.clone()),
+                depth - 1,
+                -b,
+                -a
+            )
+            if value >= b:
+                return b
+            if value > a:
+                a = value
+        return a
 
     def IDDFS(self, gamestate: gamestate.GameState):
-        self.depth = 0
-        while time.time_ns() - self.now < self.MAX_TIME:
-            self.depth += 1
-            self.alphaBeta(gamestate, self.depth, -math.inf, math.inf)
-        if self.best[self.depth][0] > self.best[self.depth-1][0]:
-            print(f"Depth: {self.depth}")
-            return self.best[self.depth]
-        print(f"Depth: {self.depth-1}")
-        return self.best[self.depth-1]
+        possible_moves = gamestate.get_possible_moves()
+        depth = 0
+        values = {}
+        self.timeout = False
+        while not self.timeout:
+            for move in possible_moves:
+                value = self.alphaBeta(
+                    move.perform(gamestate.clone()),
+                    depth,
+                    -math.inf,
+                    math.inf
+                )
+                if self.timeout:
+                    break
+                values[move] = value
+            possible_moves = sorted(
+                possible_moves,
+                key=lambda m: values.get(m, -math.inf),
+                reverse=True
+            )
+            depth += 1
+        print("d", depth, "e", values[possible_moves[0]], end=" ")
+        return possible_moves[0]
 
     def evaluate(self, gamestate):
         value = (
-            -gamestate.pieces_around_bee(self.color)
-            + gamestate.pieces_around_bee(self.opp)
+            -gamestate.pieces_around_bee(gamestate.color)
+            + gamestate.pieces_around_bee(gamestate.opp)
         )
-        if gamestate.color != self.color:
-            return -value
         return value
 
     def get(self, gamestate: gamestate.GameState) -> moves.Move:
         self.now = time.time_ns()
 
-        self.best = {}
         self.color = gamestate.color
         self.opp = gamestate.opp
 
         move = self.IDDFS(gamestate)
 
         then = time.time_ns() - self.now
-        print(f"Time: {round(then/1000000000, 2)} s")
-        print(f"Eval: {move[0]}")
-        return move[1]
+        print("t", round(then/1000000000, 2))
+        return move
